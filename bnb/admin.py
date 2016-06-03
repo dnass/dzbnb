c6883c524df7bf0aed0d6912e51258afcaa18638
dnass <dnlnss@gmail.com>
2016-06-03 16:48:55 +0700

    Added user, reservation, review models

--- bnb/admin.py
+++ bnb/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Property, View
+from .models import BNBUser, Property, Reservation, Review, View
 
+admin.site.register(BNBUser)
 admin.site.register(Property)
+admin.site.register(Reservation)
+admin.site.register(Review)
 admin.site.register(View)
