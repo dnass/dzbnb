56320cfbc540be3d010fa1dccef45bd253728156
Zhengyu Ren <renzhengyu@gmail.com>
2016-06-08 12:27:55 +0700

    homepage completed
    To code: edit/hide/unhide property. Commit now so Dan and I don't fork
    too differently.

---
+++ bnb/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.homepage, name='homepage'), # ex: /dzbnb/
+    url(r'^reservation/(?P<reservation_id>[0-9]+)/approve/$', views.approve_reservation, name='approve_reservation'), # ex: /dzbnb/reservation/123/approve
+    url(r'^property/(?P<propertie_id>[0-9]+)/$', views.propertie, name='property_details'), # ex: /dzbnb/property/123
+    url(r'^property/(?P<propertie_id>[0-9]+)/request/$', views.request_reservation, name='request_reservation'), # ex: /dzbnb/property/123/request
+    url(r'^property/(?P<propertie_id>[0-9]+)/edit/$', views.edit_property, name='edit_property'), # ex: /dzbnb/property/123/edit
+    url(r'^property/(?P<propertie_id>[0-9]+)/hide/$', views.hide_property, name='hide_property'), # ex: /dzbnb/property/123/hide
+    url(r'^property/(?P<propertie_id>[0-9]+)/unhide/$', views.unhide_property, name='unhide_property'), # ex: /dzbnb/property/123/unhide
+    url(r'^user/(?P<bnbuser_id>[0-9]+)/$', views.bnbuser_details, name='bnbuser_details'), # ex: /dzbnb/user/123
+]
--- bnb/views.py
+++ bnb/views.py
@@ -1,3 +1,87 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseForbidden
+from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
+from .models import *
+from django.utils.translation import ugettext_lazy as _
+from django.db.models import Avg, Count
 
-# Create your views here.
+
+
+def homepage(request):
+    if request.user.is_authenticated():
+        recently_added_properties = Propertie.objects.order_by('-creation_date')[:10]
+        reservation_requests = Reservation.objects.filter(propertie__owner__user_id=request.user.id).filter(approved=False).order_by('-start_date')
+        context = {
+            'bnbuser': BNBUser.objects.filter(user__id=request.user.id)[0],
+            'recently_added_properties': recently_added_properties,
+            'reservation_requests': reservation_requests,
+        }
+        return render(request, 'bnb/homepage.html', context)
+    else:
+        context = {'return_page': 'homepage'}
+        return render(request, 'bnb/login.html', context)
+
+def approve_reservation(request, reservation_id):
+    try:
+        r = Reservation.objects.get(pk=reservation_id)
+        if request.user.is_authenticated() and (r.propertie.owner.user == request.user or request.user.is_superuser):
+            if r.approved:
+                warning = "It had been approved, though."
+            else:
+                warning = ""
+                r.approved = True
+                r.save()
+        else:
+            raise PermissionDenied
+    except ObjectDoesNotExist:
+        # invalid reservation_id
+        return HttpResponseNotFound(_('Reservation does not exist.'))
+    except PermissionDenied:
+        # user is not authrised to approve the reservation
+        return HttpResponseForbidden(_('Permission Denied.'))
+    except:
+        # unexpected error
+        return HttpResponse(_('An unexpected error occured.'))
+    else:
+        # all code excuted which means approve action successful
+        return HttpResponse(_('Reservation [{}] is approved. {}'.format(reservation_id, warning)))
+
+def propertie(request, propertie_id):
+    try:
+        p = Propertie.objects.annotate(average_rating=Avg('review__rating'), rating_count=Count('review')).get(pk=propertie_id)
+        if p.hidden and not request.user.is_superuser:
+            raise ObjectDoesNotExist
+        else:
+            
+            context = {
+                'p': p,
+                'show_reservation_form': p.owner.user != request.user,
+                'show_admin_links': request.user.is_superuser or p.owner.user == request.user,
+                'requested_by': request.user.id,
+                'reviews': Review.objects.filter(propertie__owner__id=p.owner.id).filter(hidden=False)
+            }
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound(_('Property not found'))
+    except:
+        # unexpected error
+        return HttpResponse(_('An unexpected error occured.'))
+    else:
+        return render(request, 'bnb/property.html', context)
+
+def request_reservation(request, propertie_id):
+    #request.POST.get('start_date')
+    #request.POST.get('end_date')
+    return HttpResponseNotFound('to be coded.')
+
+def edit_property(request, propertie_id):
+    return HttpResponseNotFound('to be coded.')
+
+def hide_property(request, propertie_id):
+    return HttpResponseNotFound('to be coded.')
+
+def unhide_property(request, propertie_id):
+    return HttpResponseNotFound('to be coded.')
+
+def bnbuser_details(request, bnbuser_id):
+    return HttpResponseNotFound('to be coded.')
+
